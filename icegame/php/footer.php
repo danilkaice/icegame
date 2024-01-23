0dd806883d37e206dcb4d3074de51d98136a36b2
danilkaice <115411194+danilkaice@users.noreply.github.com>
2024-01-23 10:15:32 +0500

    version 0.3

---
+++ icegame/php/footer.php
@@ -0,0 +1,15 @@
+<footer>
+    <div class="foot">
+        <!-- логотип -->
+        <div class="namelogo">
+            <span id="nameBlog">IceGame</span>
+            <span>by TwentyOneIce Studio</span>
+        </div>
+        <!-- права -->
+        <div class="rights">
+            <p>IceGame - блог о играх от разработчиков.</p>
+            <a href="https://t.me/danilkaicejami"><img src="img/telegram.png">Наш телеграм</a>
+            <span>©TwentyOneIce Company</span>
+        </div>
+    </div>
+</footer>
