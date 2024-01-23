a002d606837c12bf38dd937839e8935a5798a9bf
danilkaice <115411194+danilkaice@users.noreply.github.com>
2024-01-23 10:09:58 +0500

    version 0.3

---
+++ icegame/main.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="icon" href="img/logo.png">
+    <title>IceGame</title>
+</head>
+<body>
+    <div class="wrapper">
+        <!-- шапка -->
+        <?php
+            require 'php/header.php';
+        ?>
+
+        <!-- основная секция -->
+        <section>
+            <div class="main">
+                <div class="posts">
+                    <!-- вывод постов -->
+                    <?php
+                        require 'php/selectPosts.php';
+                    ?>
+                </div>
+            </div>
+        </section>
+
+        <!-- подвал -->
+        <?php
+            require 'php/footer.php';
+        ?>
+    </div>
+</body>
+</html>
